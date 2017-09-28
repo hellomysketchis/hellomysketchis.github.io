0a1833785088f35d399a4db67b6ef0e49f634a29
Migue <minzdesigner@gmail.com>
2017-09-28 01:11:39 -0500

    add suport to scrolled in links

--- src/js/libs/scrolled.js
+++ src/js/libs/scrolled.js
@@ -6,7 +6,7 @@ var $btn_scroll = $('.scroll');
   $btn_scroll.on('click', function(e){
     e.preventDefault();
 
-    $('body').animate({scrollTop: $(this.hash).position().top -65}, 800, 'easeInOutCirc');
+    $('html, body').animate({scrollTop: $(this.hash).position().top -65}, 800, 'easeInOutCirc');
     
     $('.scroll.is-current').removeClass('is-current');
     $(this).addClass('is-current');
